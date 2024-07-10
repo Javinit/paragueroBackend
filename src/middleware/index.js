1a33c1b6b0df75c410ec6f3f05f31a85c7fc2751
Alejandro Rojas <alejandro@h2d.dev>
2024-07-10 04:40:22 -0400

    aaa

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -14,3 +14,13 @@ export const authVerify = async (req, res, next) => {
     req.user = user
     next()
 }
+
+export const getInfo = async (req, res, next) => {
+    const { authorization } = req.headers
+    if (!authorization) return next()
+
+    const infoUser = await verifyJWT(authorization)
+    const user = await User.findOne({ email: infoUser.email }, { password: 0 })
+    req.user = user
+    next()
+}
